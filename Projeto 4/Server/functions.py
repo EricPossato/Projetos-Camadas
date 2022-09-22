decc316f871077157abdebc64311b5fc1b546a2a
EricPossato <ericalp@al.insper.edu.br>
2022-09-22 11:28:38 -0300

    write_log

--- Projeto 4/Server/functions.py
+++ Projeto 4/Server/functions.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 def head_generator(h0=0, h1=0, h2=0, h3=0, h4=0, h5=0, h6=0, h7=0, h8=0, h9=0):
     head = h0.to_bytes(1, byteorder='little') + h1.to_bytes(1, byteorder='little') + h2.to_bytes(1, byteorder='little') + h3.to_bytes(1, byteorder='little') + h4.to_bytes(1, byteorder='little') + h5.to_bytes(1, byteorder='little') + h6.to_bytes(1, byteorder='little') + h7.to_bytes(1, byteorder='little') + h8.to_bytes(1, byteorder='little') + h9.to_bytes(1, byteorder='little')
     return head
@@ -23,3 +24,10 @@ def package_generator(h0, h3, h4, h5, h6, h7, payload):
     head = head_generator(h0=h0, h3=h3, h4=h4, h5=h5, h6=h6, h7=h7)
     EOP = b'\xAA\xBB\xCC\xDD'
     return head + payload + EOP
+
+def write_log (string, envio, tipo, tamanho, indice, total_packages):
+    now = datetime.now()
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+    log = f'{dt_string} / {envio}/{tipo}/{tamanho}/{indice}{total_packages}\n'
+    string += log
+    return string
